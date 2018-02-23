bdea5e7e4fc8eabc1c4dabf66e47f3d19b6096dd
pranz1996 <patelpranzp25@gmail.com>
2018-02-23 18:08:24 +0530

    Update OrganizationBoard.java

--- organization/OrganizationBoard.java
+++ organization/OrganizationBoard.java
@@ -1,1 +1,37 @@
+package organization;
 
+import java.io.*;
+// import java.sql.*;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.*;
+
+@WebServlet("/OrganizationBoard")
+public class OrganizationBoard extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+    
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		// UserDB(DAO) object
+		UserDB userDB = new UserDB();		
+		
+		// Fetch all tasks details
+		List<Task> list = new ArrayList<>();
+		
+		// getting all tasks with details
+		list = userDB.getAllDetails();
+		
+		// setting list 
+		request.setAttribute("LIST", list);
+		
+		// respond to view page
+		RequestDispatcher dispatch = request.getRequestDispatcher("/organizationBoard.jsp");
+		dispatch.forward(request, response);
+	}
+
+}
