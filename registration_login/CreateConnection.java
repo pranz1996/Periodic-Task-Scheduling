bda9ccfa4d7583b69b2ca6d3da9ce6cdb382535d
pranz1996 <patelpranzp25@gmail.com>
2018-02-23 09:37:46 +0530

    Update CreateConnection.java

--- registration_login/CreateConnection.java
+++ registration_login/CreateConnection.java
@@ -1,1 +1,24 @@
+package registration_login;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class CreateConnection {
+	public static Connection ConnectionGet() {
+		Connection conn = null ;
+		
+		// url of database schema 'Periodic Task Scheduling'
+		String url = "jdbc:mysql://localhost:3306/periodic task scheduling";
+		try {
+			// Registering mysql Driver
+			Class.forName("com.mysql.jdbc.Driver");
+			
+			// Connection establishment
+			conn = DriverManager.getConnection(url,"root","root");
+		}
+		catch(Exception e) {
+			System.out.println(e);
+		}
+		return conn;
+	}
+}
