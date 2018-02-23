4806b49bebc3c5b7932c371cab2601514df3d9cf
pranz1996 <patelpranzp25@gmail.com>
2018-02-23 17:08:57 +0530

    Update ListResetClass.java

--- individual.user/ListResetClass.java
+++ individual.user/ListResetClass.java
@@ -1,1 +1,27 @@
+package individual.user;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListResetClass {
+
+	public List<GetSet> set(List<GetSet> tasks) {
+		
+		// if task is not part of label then don't include in list
+		List<GetSet> list = null ; 
+		list = new ArrayList<>();
+		
+		for(GetSet li : tasks ) {
+			if(li.getLabel().equals("")){
+				continue;
+			}
+			else if(li.getLabel()==null) {
+					continue;
+			}else {
+				list.add(li);
+			}
+		}
+		return list;
+	}
+	
+}
